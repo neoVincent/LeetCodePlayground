e8bb1b285300eb23c8be344f3c7a2a4f5a84cdb5
vincent <neovincent@163.com>
2018-04-06 09:52:44 +0800

    Solution for LeetCode 66 Plus one accepted! 98.13

--- LC_66_Plusone/main.cpp
+++ LC_66_Plusone/main.cpp
@@ -1,4 +1,31 @@
 #include <QCoreApplication>
+#include <vector>
+#include <QDebug>
+
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits)
+{
+    int size = digits.size();
+    while(!digits.empty())
+    {
+        if(digits.back() == 9)
+        {
+            digits.pop_back();
+        }
+        else
+        {
+            int t = digits.back();
+            digits.pop_back();
+            digits.push_back(t+1);
+            break;
+        }
+    }
+   int diffSize = size - digits.size();
+   if (diffSize == size) digits.push_back(1);
+   for(int i = 0; i< diffSize ; i++) digits.push_back(0);
+   return digits;
+}
 
 int main(int argc, char *argv[])
 {
