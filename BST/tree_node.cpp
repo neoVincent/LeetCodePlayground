907b3f355edd852abfa4a4cec49f09e3579214e3
vincent <neovincent@163.com>
2018-04-21 12:11:55 +0800

    Add BST operations

--- BST/tree_node.cpp
+++ BST/tree_node.cpp
@@ -11,6 +11,18 @@ void printNode(TreeNode* node)
     cout << "  " << node->val << "  ";
 }
 
+TreeNode* findParent(TreeNode* root, TreeNode* node)
+{
+    TreeNode* parent = nullptr;
+    while(root && root->val != node->val)
+    {
+        parent = root;
+        if (root->val > node->val) root = root->left;
+        else if (root->val < node->val) root = root->right;
+    }
+    return parent;
+}
+
 void inorderTreeWalk(TreeNode *tree)
 {
     if (tree != nullptr)
@@ -146,4 +158,66 @@ TreeNode *maximum(TreeNode *tree)
     TreeNode* node = tree;
     while(node && node->right) node = node->right;
     return node;
+}
+
+void treeInsert(TreeNode *tree, TreeNode *node)
+{
+    //Tree and node should not be null
+    if (!node || !tree) return;
+
+    TreeNode* parent = nullptr;
+    while(tree)
+    {
+        parent = root;
+        if (tree->val > node->val)
+        {
+            tree = tree->left;
+        }
+        else if(tree->val < node->val)
+        {
+            tree = tree->right;
+        }
+    }
+
+    //parent should not be null becase tree is not null
+    if (parent->val > node->val) parent->left = node;
+    else parent->right = node;
+}
+
+void treeDelete(TreeNode *tree, TreeNode *node)
+{
+    if (!node->left)
+    {
+        transplant(tree,node->right,node);
+    }
+    else if (!node->right)
+    {
+        transplant(tree,node->left,node);
+    }
+    else
+    {
+        TreeNode* successor = treeSuccessor(tree,node);
+        if (successor != node->left && successor != node->right)
+        {
+            transplant(tree,successor->right,successor);
+            successor->right = node->right;
+        }
+
+        transplant(tree,successor,node);
+        successor->left = node->left;
+    }
+}
+
+void transplant(TreeNode *tree, TreeNode *from, TreeNode *to)
+{
+    TreeNode* p = findParent(tree,to);
+    if (!p) return;
+    if (p->left == to)
+    {
+        p->left = from;
+    }
+    else
+    {
+        p->right = to;
+    }
 }
