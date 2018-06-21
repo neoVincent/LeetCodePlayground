ce4ccbac21aa7345c7a220b2c2086c52684ed13a
vincent <neovincent@163.com>
2018-06-21 16:44:56 +0800

    Complete LC 53 Maxium subarray

--- LC_53_MaximumSubarray/main.cpp
+++ LC_53_MaximumSubarray/main.cpp
@@ -8,13 +8,23 @@ using namespace std;
 
 int maxSubArray(vector<int>& nums)
 {
-
+    if (nums.size() == 1) return nums[0];
+    vector<int> sum(nums.size());
+    sum[0] = nums[0];
+    int res = sum[0];
+    for(int i = 1; i < nums.size(); i++)
+    {
+        sum[i] = max(sum[i-1] + nums[i],nums[i]);
+        if (sum[i] > res) res = sum[i];
+    }
+    return res;
 }
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    vector<int> nums{-1,-2};
+    cout << "Result: " << maxSubArray(nums) <<endl;
     return a.exec();
 }
