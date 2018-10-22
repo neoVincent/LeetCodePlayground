8f7c5a2962665ba387e09db9d4805dc483754bba
Kaiyi <neovincent@163.com>
2018-10-22 14:55:31 -0400

    Add 2 more methods for link palindrome, the recursion is awsome!

--- LC_234_PalindromList/main.cpp
+++ LC_234_PalindromList/main.cpp
@@ -1,12 +1,20 @@
 #include <QCoreApplication>
 #include <stack>
 #include <queue>
+#include <iostream>
+
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr){}
+};
+
+struct ResultNode
+{
+    bool isPalindrome;
+    ListNode* node;
 };
 
 bool isPalindrome(ListNode* head)
@@ -28,47 +36,93 @@ bool isPalindrome(ListNode* head)
         qu.pop();
     }
     return true;
-
-//    int cnt=0;
-//           if(head==NULL||head->next==NULL) return true;
-//           ListNode *p=head;
-//           while(p!=NULL)
-//           {
-//               ++cnt;
-//               p=p->next;
-//           }
-//          // cout<<"cnt="<<cnt<<endl;
-//           int n=cnt/2;
-//           for(p=head;n>1;--n)
-//           {
-//               ListNode *tmp=p->next;
-//               //head=p->next;
-//               p->next=tmp->next;
-//               tmp->next=head;
-//               head=tmp;
-//           }
-//           p=p->next;
-//           if(cnt%2==1)
-//               p=p->next;
-//           //cout<<"p="<<p->val<<" head="<<head->val<<endl;
-//           //cout<<"cnt/="<<n-1<<endl;
-//           for(int i=0;i<cnt/2;++i)
-//           {
-//              // cout<<"p="<<p->val<<" head="<<head->val<<endl;
-//               if(p->val!=head->val)
-//                   return false;
-
-//               p=p->next;
-//               head=head->next;
-//           }
-//           return true;
 }
 
+//Iterate
+bool isPalindrome2(ListNode* head)
+{
+
+    //use runner pointer
+    if (!head) return true;
+    ListNode* iter = head;
+    ListNode* p1 = head;
+    stack<ListNode*> firstHalf;
+    while(iter && iter->next)
+    {
+        firstHalf.push(p1);
+        p1 = p1->next;
+        iter = iter->next->next;
+    }
+
+    //different case: if the length is odd, p1 go to next
+    if (iter)
+    {
+        p1 = p1->next;
+    }
+
+    while(!firstHalf.empty())
+    {
+        auto p = firstHalf.top();
+        firstHalf.pop();
+        if (p->val != p1->val) return false;
+        p1 = p1->next;
+    }
+    return true;
+}
+
+ResultNode recurse(ListNode* head, int length)
+{
+    //arrive at the middle
+    //even number
+    if (head == nullptr || length <= 0)
+    {
+        return {true,head};
+    }
+    else if (length == 1)
+    {
+        return {true,head->next};
+    }
+
+    ResultNode result = recurse(head->next,length-2);
+    if (!result.isPalindrome || !result.node)
+    {
+        return result;
+    }
+
+    result.isPalindrome = (result.node->val == head->val);
+    result.node = result.node->next;
+
+    return result;
+}
+
+int listSize(ListNode* head)
+{
+    ListNode* p = head;
+    int size = 0;
+    while(p)
+    {
+        size++;
+        p = p->next;
+    }
+    return size;
+}
+
+//Recurse
+bool isPalindrome3(ListNode* head)
+{
+    int size = listSize(head);
+    return recurse(head,size).isPalindrome;
+}
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    ListNode* l3 = new ListNode(1);
+    ListNode* l2 = new ListNode(0);
+    ListNode* l1 = new ListNode(1);
+    l1->next = l2;
+    l2->next = l3;
+    cout << isPalindrome3(l1)<< endl;
     return a.exec();
 }
